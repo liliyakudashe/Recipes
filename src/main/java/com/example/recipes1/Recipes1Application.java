54da495c43515a2509627c9c4d08a58fc2f1e3df
Acer <oduvan4ik_888@mail.ru>
2022-12-11 16:27:35 +0300

    Первый

---
+++ src/main/java/com/example/recipes1/Recipes1Application.java
@@ -0,0 +1,13 @@
+package com.example.recipes1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipes1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipes1Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/recipes1/controllers/FirstControiiers.java
@@ -0,0 +1,23 @@
+package com.example.recipes1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class FirstControiiers {
+
+    @GetMapping
+    public String theApplicationIsRunning() {
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String info(){
+        return "Имя ученика - Лилия\n " +
+                "Название проекта - книга рецептов\n " +
+                "Дата создания проекта - 11 декабрь 2022 год\n " +
+                "Описание проекта - сайт с рецептами";
+    }
+
+}
